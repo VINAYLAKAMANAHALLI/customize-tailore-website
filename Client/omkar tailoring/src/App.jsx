1abedf6d43f313a350912f5025cc91a2be1f143f
VINAYLAKAMANAHALLI <vinaylakamanahalli@gmail.com>
2026-05-28 13:50:15 +0530

    initial project

---
+++ Client/omkar tailoring/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-red-900 min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">App</h1>
+    </div>
+  ) 
+}
+
+export default App
